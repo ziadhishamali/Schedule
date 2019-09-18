8f8ec9176fa9c987d2309635c4548dd8a76f0f02
Ziad Hisham Ali <ziadhesham97@gmail.com>
2019-09-18 21:59:01 +0200

    initial commit

---
+++ src/components/Row.js
@@ -0,0 +1,15 @@
+class Row extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+    }
+    render() { 
+        return ( 
+
+        );
+    }
+}
+ 
+export default Row;
---
+++ src/components/Table.js
@@ -0,0 +1,15 @@
+class Table extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+
+        }
+    }
+    render() { 
+        return ( 
+
+        );
+    }
+}
+ 
+export default Table;
