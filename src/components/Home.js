30538dd26b27e5b725fe96cfef8c6a9e6ce2548d
Ziad Hisham Ali <ziadhesham97@gmail.com>
2019-09-19 15:29:59 +0200

    style update

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,6 +23,15 @@ class Home extends Component {
         });
     }
 
+    sleep = (milliseconds) => {
+        var start = new Date().getTime();
+        for (var i = 0; i < 1e7; i++) {
+          if ((new Date().getTime() - start) > milliseconds){
+            break;
+          }
+        }
+      }
+
     componentDidMount() {
         const db = firebase.firestore();
         console.log(db);
--- src/components/views/Content.js
+++ src/components/views/Content.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Loader from './Loader';
 
 const getAll = (content, type) => {
     return content.map((element, idx) => {
@@ -10,40 +11,44 @@ const getAll = (content, type) => {
 
 const Content = ({contents}) => {
 
-    const contentTable = contents ? ( contents.map((content, idx) => {
-        let background = "";
-        let today = new Date().getDay() + 1;
-        if (today === 7) {
-            today = 0;
-        }
-        let tomorrow = today + 1;
-        if (tomorrow === 7) {
-            tomorrow = 6;
-        }
-        if (idx === today) {
-            background = " blue-background-2 white-text";
-        }
-        if (idx === tomorrow) {
-            background = " blue-background-4 white-text";
-        }
-        if (idx === 5) {
-            background = " green-background-2 white-text";
-        }
-        return (
-            <div key={idx} className="flex-row justify align">
-                <div className={"content-name flex-column justify align box-shadow black-text" + background}>
-                    {getAll(content, "name")}
+    const contentTable = contents ? (
+        contents.map((content, idx) => {
+            let background = "";
+            let today = new Date().getDay() + 1;
+            if (today === 7) {
+                today = 0;
+            }
+            let tomorrow = today + 1;
+            if (tomorrow === 7) {
+                tomorrow = 6;
+            }
+            if (idx === today) {
+                background = " blue-background-2 white-text";
+            }
+            if (idx === tomorrow) {
+                background = " blue-background-4 white-text";
+            }
+            if (idx === 5) {
+                background = " green-background-2 white-text";
+            }
+            return (
+                <div key={idx} className="flex-row justify align">
+                    <div className={"content-name flex-column justify align box-shadow black-text" + background}>
+                        {getAll(content, "name")}
+                    </div>
+                    <div className={"content-instructor flex-column justify align box-shadow black-text" + background}>
+                        {getAll(content, "instructor")}
+                    </div>
+                    <div className={"content-type flex-column justify align box-shadow black-text" + background}>
+                        {getAll(content, "type")}
+                    </div>
                 </div>
-                <div className={"content-instructor flex-column justify align box-shadow black-text" + background}>
-                    {getAll(content, "instructor")}
-                </div>
-                <div className={"content-type flex-column justify align box-shadow black-text" + background}>
-                    {getAll(content, "type")}
-                </div>
-            </div>
-        )
-    }) ) : (
-        <div></div>
+            )
+        })
+    ) : (
+        new Array(6).fill(0).map((ele, idx) => {
+            return <Loader key={idx} />
+        })
     )
 
     return (
