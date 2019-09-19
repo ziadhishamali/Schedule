0cfd1235069b66045051016a0026c744b86a4e3b
Ziad Hisham Ali <ziadhesham97@gmail.com>
2019-09-19 16:40:12 +0200

    Updates

---
+++ precache-manifest.62887cad9d4a3ae5c0b56b46c74571a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7410f600c152e893217bcd9543370484",
+    "url": "/schedule/index.html"
+  },
+  {
+    "revision": "997f95f290e8747d60d5",
+    "url": "/schedule/static/css/main.df678031.chunk.css"
+  },
+  {
+    "revision": "be518ba4853ae47b8a66",
+    "url": "/schedule/static/js/2.dc58868b.chunk.js"
+  },
+  {
+    "revision": "997f95f290e8747d60d5",
+    "url": "/schedule/static/js/main.603e6d69.chunk.js"
+  },
+  {
+    "revision": "4b8cc93c7b24cb1b3928",
+    "url": "/schedule/static/js/runtime~main.cdef8864.js"
+  }
+]);
